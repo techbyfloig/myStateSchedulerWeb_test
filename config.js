911eec167cc1f94040703302ce568e8db863ba60
Kenny Roig Arcia <kennyroig@techbyfloig.com>
2026-03-29 13:24:07 -0500

    adding code first time

---
+++ config.js
@@ -0,0 +1,39 @@
+const CONFIG = {
+  app: {
+    name: "My State Scheduler",
+    mobileName: "My State Scheduler",
+    icon: "appicon.jpg",
+  },
+  company: {
+    name: "Tech by Floig LLC",
+    street: "4853 Preston Trail Dr",
+    city: "Mesquite",
+    state: "TX",
+    zip: "75150",
+    country: "United States",
+    email: "support@techbyfloig.com",
+    copyrightYear: "2026",
+  },
+  links: {
+    formspree: "https://formspree.io/f/mreawawg",
+    appStore: "#",
+    playStore: "#",
+    twitter: "#",
+    instagram: "#",
+    facebook: "#",
+  },
+  stats: {
+    appointmentsMonitored: "10000",
+    appRating: "4.8",
+    usersServed: "2500",
+    avgTimeSaved: "48",
+    dpsLocations: "150",
+  },
+  pricing: {
+    freePrice: "$0",
+  },
+  legal: {
+    effectiveDate: "March 1, 2026",
+    effectiveDateEs: "1 de marzo de 2026",
+  },
+};
